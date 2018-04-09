14e5331dba7de9d0be5866a2fe50e5e73517fa5d
Maria Samaniego <mariarsaman@gmail.com>
2018-04-09 17:38:17 -0500

    continued work on user-signup

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), a
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route('/validate-user')
+@app.route('/')
 def index():
     template = jinja_env.get_template('user_form.html')
     return template.render()
@@ -18,57 +18,62 @@ def index():
 #   validation criteria: 
 #       fields 1-3 cannot be left empty
 
-def required_field(string)
-    if string = ''
-        return False
-    else:
+
+def required_field(string):
+    if len(string) > 0:
         return True
 
 #       username and password cannot contain space character, ex: be less than 3 or more than 20 char
 
-def valid_char_count(string)
-    if string <= 20 or string >= 3:
+def valid_char_count(string):
+    if len(string) <= 20 and len(string) >= 3:
         return True
     else:
         return False
 
-def no_space_string(string)
-    if char in string==''
-        return False
+def no_space_string(string):
+    if char in string !=' ':
+        return True
     else:
-        return True 
+        return False 
 
 #       password and password confrimation do not match
 
-def valid_password_conf(password, password_conf)
-    if password = password-conf:
+def valid_password_conf(password, password_conf):
+    if password == password_conf:
         return True
     else:
         return False
 
 #       IF email field filled in, must have 3-20 char no space, a single @ and a single .
-def 
+#def valid_email(string)
+#    if char in string == char.isalnum or
 
 
 # each feedback message should be next to the field it refers to
 
 
-@app.route('/validate-user', methods=['POST'])
+@app.route('/', methods=['POST'])
 def validate_user():
     username=request.form['user-name']
+    
+    if user-name is not required_field(string) or user-name is not valid_char_count(string) or user-name is not no_space_string(string):
+        error = "That's not a valid username"
+        return redirect("/?error=" + error)
+
     password=request.form['password']
     verify_passowrd=request.form['password-conf']
     email=request.email['email']
 
 username_error=''
-password_error=''
-password_conf_error=''
-email_error=''
+#password_error=''
+#password_conf_error=''
+#email_error=''
 
 
 
     
-# if input for username and email are valid, preserve what user tuped
+# if input for username and email are valid, preserve what user typed
 
 ##display a welcome message of "Welcome [username]"
 
